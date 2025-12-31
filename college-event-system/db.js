8d0e635c3ccded32e6a2b41ef8f337abda65292d
Rishav-Kundu <rishavzeus04@gmail.com>
2026-01-01 01:35:38 +0530

    Update db.js

--- college-event-system/db.js
+++ college-event-system/db.js
@@ -10,10 +10,11 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
-    console.error("❌ MySQL connection failed:", err.message);
+    console.error("❌ MySQL connection failed:", err);
   } else {
     console.log("✅ Connected to Railway MySQL");
   }
 });
 
 module.exports = db;
+
